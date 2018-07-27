c7e1e2571f16296e3395efa863f77e62c0b69266
yan <33154558+leonyan18@users.noreply.github.com>
2018-07-27 22:41:38 +0800

    7.27

--- test.cpp
+++ test.cpp
@@ -1,48 +1,121 @@
-#include <iostream>
-#include <cstring>
-#include <cstdio>
-#include <queue>
-#include <algorithm>
-#include <cmath>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-char b[1234567];
-int Next[1234567];
-int cnt;
-int n;
-
-
-void SetNext()//子串的next数组，如果只看代码的话，很难得出值的确定方法。
+#define Lson l,m,rt<<1
+#define Rson m+1,r,rt<<1|1
+#define pll pair<int,int>
+#define mp make_pair
+#define ll long long
+#define INF 0x3f3f3f3f
+const int maxn=1e5+5;
+int n,q;
+struct node
 {
-    int i=0,j=-1;
-    Next[0]=-1;
-    while(i<n)
+    int cnt,addv,minb,maxa;
+} tree[maxn<<2];
+int b[maxn];
+void push_up(int rt)
+{
+    tree[rt].minb=min(tree[rt<<1].minb,tree[rt<<1|1].minb);
+    tree[rt].cnt=tree[rt<<1].cnt+tree[rt<<1|1].cnt;
+    tree[rt].maxa=max(tree[rt<<1].maxa,tree[rt<<1|1].maxa);
+}
+void push_down(int rt)
+{
+    if(tree[rt].addv)
     {
-        if(j==-1||b[i]==b[j])//b为模式数组
-        {
-            i++; j++;
-            Next[i]=j;
-        }
-        else
-        j=Next[j];
+        int v=tree[rt].addv;
+        tree[rt].addv=0;
+        tree[rt<<1].maxa+=v;
+        tree[rt<<1|1].maxa+=v;
+        tree[rt<<1].addv+=v;
+        tree[rt<<1|1].addv+=v;
     }
 }
-
+void build(int l,int r,int rt)
+{
+    tree[rt].addv=0;
+    if(l==r)
+    {
+        tree[rt].cnt=tree[rt].maxa=0;
+        tree[rt].minb=b[l];
+        return;
+    }
+    int m=l+r>>1;
+    build(Lson);
+    build(Rson);
+    push_up(rt);
+}
+void updata(int L,int R,int l,int r,int rt)
+{
+    if(L<=l&&r<=R)
+    {
+        tree[rt].maxa++;
+        if(tree[rt].maxa<tree[rt].minb)
+        {
+            tree[rt].addv++;
+            return;
+        }
+        if(l==r&&tree[rt].maxa>=tree[rt].minb)
+        {
+            tree[rt].cnt++;
+            tree[rt].minb+=b[l];
+            return;
+        }
+    }
+    push_down(rt);
+    int m=l+r>>1;
+    if(L<=m)
+        updata(L,R,Lson);
+    if(R>m)
+        updata(L,R,Rson);
+    push_up(rt);
+}
+int query(int L,int R,int l,int r,int rt)
+{
+    if(L<=l&&r<=R)
+    {
+        return tree[rt].cnt;
+    }
+    int m=l+r>>1;
+    push_down(rt);
+    int ans=0;
+    if(L<=m)
+        ans+=query(L,R,Lson);
+    if(R>m)
+        ans+=query(L,R,Rson);
+    return ans;
+}
 int main()
 {
-    int t = 1;
-    while(scanf("%d", &n) && n)
+	#ifdef ONLINE_JUDGE
+	#else
+    freopen("C://Users//yan//Desktop//in.txt","r",stdin);
+	#endif
+    int n,x,y;
+    while(~scanf("%d %d",&n,&q))
     {
-        scanf("%s", b);
-        SetNext();
-        cout <<"Test case #" <<t++<<endl;
-            //cout << Next[i] << endl;
-            int length = n-Next[n];  //循环节的长度
-            //cout << length << endl;
-            if(n != length && n% length == 0)
-                printf("%d\n", length);
-        printf("\n");
+        for(int i=1; i<=n; i++)
+        {
+            scanf("%d",&b[i]);
+        }
+        build(1,n,1);
+        char pp[6];
+        int l,r;
+        while(q--)
+        {
+            scanf("%s%d%d",pp,&l,&r);
+            if(pp[0]=='a')
+            {
+                updata(l,r,1,n,1);
+            }
+            else
+            {
+                printf("%d\n",query(l,r,1,n,1));
+            }
+        }
     }
+    for(int i=0;i<4*n;i++){
+		cout<<tree[i].addv<<" "<<tree[i].maxa<<" "<<tree[i].minb<<" "<<tree[i].cnt<<endl;
+	}
     return 0;
 }
--- 杭电多校2G.cpp
+++ 杭电多校2G.cpp
@@ -1,111 +1,115 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+const double PI=acos(-1.0);
+typedef long long ll;
 #define rep(i,a,n) for (int i=a;i<n;i++)
 #define per(i,a,n) for (int i=n-1;i>=a;i--)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(),(x).end()
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
-typedef vector<int> VI;
-typedef long long ll;
-typedef pair<int,int> PII;
-const ll mod=1000000007;
-ll powmod(ll a,ll b) {ll res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
-ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
-// head
- 
-const int N=101000;
-struct node {
-	int fg;//标记 
-	int s;
-	int mv;//最小值 
-}nd[4*N];
-int b[N],n,m,l,r;
-char s[20];
- 
-void upd(int p) {
-	nd[p].mv=min(nd[p+p].mv,nd[p+p+1].mv);
-	nd[p].s=nd[p+p].s+nd[p+p+1].s;
+#define clr(x) memset(x,0,sizeof(x))
+const int INF=0x3f3f3f3f;
+const int maxn=1e5+5;
+struct point{
+	int maxa;
+	int minb;
+	int flag;
+	int sum;
+}tree[maxn<<2];
+int b[maxn];
+void pushup(int pos){
+	tree[pos].maxa=max(tree[pos<<1].maxa,tree[pos<<1|1].maxa);
+	tree[pos].minb=min(tree[pos<<1].minb,tree[pos<<1|1].minb);
+	tree[pos].sum=tree[pos<<1].sum+tree[pos<<1|1].sum;
 }
-void setf(int p,int f) {
-	nd[p].fg+=f;
-	nd[p].mv+=f;
-}
-void build(int p,int l,int r) {
-	nd[p].fg=0;
-	if (l==r) {
-		nd[p].mv=b[l]-1;
-		nd[p].s=0;
-	} else {
-		int md=(l+r)>>1;
-		build(p+p,l,md);
-		build(p+p+1,md+1,r);
-		upd(p);
+void pushdown(int pos){
+	if(tree[pos].flag){
+		int v=tree[pos].flag;
+		tree[pos].flag=0;
+		tree[pos<<1].maxa+=v;
+		tree[pos<<1].flag+=v;
+		tree[pos<<1|1].maxa+=v;
+		tree[pos<<1|1].flag+=v;
 	}
 }
-void push(int p) {
-	if (nd[p].fg) {
-		setf(p+p,nd[p].fg);
-		setf(p+p+1,nd[p].fg);
-		nd[p].fg=0;
+void buildTree(int l,int r,int pos){
+	tree[pos].flag=0;
+	if(l==r){
+		tree[pos].maxa=0;
+		tree[pos].sum=0;
+		tree[pos].minb=b[l];
+		return;
 	}
+	int md=(l+r)>>1;
+	buildTree(l,md,pos<<1);
+	buildTree(md+1,r,pos<<1|1);
+	pushup(pos);
 }
-int query(int p,int l,int r,int tl,int tr) {
-	if (tl==l&&tr==r) return nd[p].s;
-	else {
-		push(p);
-		int md=(l+r)>>1;
-		if (tr<=md) return query(p+p,l,md,tl,tr);
-		else if (tl>md) return query(p+p+1,md+1,r,tl,tr);
-		else return query(p+p,l,md,tl,md)+query(p+p+1,md+1,r,md+1,tr);
+void update(int l,int r,int nl,int nr,int pos){
+	if(l<=nl&&nr<=r){
+		tree[pos].maxa++;
+		if(tree[pos].maxa<tree[pos].minb)
+        {
+            tree[pos].flag++;
+            return;
+        }
+        if(nl==nr&&tree[pos].maxa>=tree[pos].minb)
+        {
+            tree[pos].sum++;
+            tree[pos].minb+=b[nl];
+            return;
+        }
 	}
+	pushdown(pos);
+    int m=(nl+nr)>>1;
+    if(l<=m)
+        update(l,r,nl,m,pos<<1);
+    if(r>m)
+        update(l,r,m+1,nr,pos<<1|1);
+    pushup(pos);
 }
-void modify(int p,int l,int r,int tl,int tr) {
-	if (tl>tr) return;
-//	printf("modify %d %d %d %d %d %d\n",l,r,tl,tr,nd[p].mv,nd[p].s);
-	if (tl==l&&tr==r) {
-		if (nd[p].mv>0) {
-			nd[p].mv--;
-			nd[p].fg--;
-		} else {
-			if (tl==tr) {
-				nd[p].mv=b[l]-1;
-				nd[p].s++;
-			} else {
-				push(p);
-				int md=(l+r)>>1;
-				if (nd[p+p].mv==0) modify(p+p,l,md,tl,md);
-					else setf(p+p,-1);
-				if (nd[p+p+1].mv==0) modify(p+p+1,md+1,r,md+1,tr);
-					else setf(p+p+1,-1);
-				upd(p);
+int  query(int l,int r,int nl,int nr,int pos){
+	if(l<=nl&&nr<=r)
+    {
+        return tree[pos].sum;
+    }
+    int m=(nl+nr)>>1;
+    pushdown(pos);
+    int ans=0;
+    if(l<=m)
+        ans+=query( l, r, nl, m,pos<<1);
+    if(r>m)
+        ans+=query( l, r, m+1, nr,pos<<1|1);
+    return ans;
+}
+int main(){
+	//本地测试
+	#ifdef ONLINE_JUDGE
+	#else
+    freopen("C://Users//yan//Desktop//in.txt","r",stdin);
+	#endif
+	ios::sync_with_stdio(false);//取消同步
+	std::cin.tie(0);//解除cin与cout的绑定，进一步加快执行效率。
+	int n,m;
+	while(cin>>n>>m){
+		rep(i,1,n+1){
+			cin>>b[i];
+		}
+		buildTree(1,n,1);
+		rep(i,0,m){
+			char s[15];
+			cin>>s;
+			int l,r;
+			cin>>l>>r;
+			if(s[0]=='a'){
+				update(l,r,1,n,1);
+			}
+			else{
+				int ans=query(l,r,1,n,1);
+				cout<<ans<<endl;
 			}
 		}
-	} else {
-		push(p);
-		int md=(l+r)>>1;
-		if (tr<=md) modify(p+p,l,md,tl,tr);
-		else if (tl>md) modify(p+p+1,md+1,r,tl,tr);
-		else modify(p+p,l,md,tl,md),modify(p+p+1,md+1,r,md+1,tr);
-		upd(p);
-	}
-//	printf("ff %d %d\n",p,nd[p].s);
-}
- 
-int main() {
-	while (scanf("%d%d",&n,&m)!=EOF) {
-		rep(i,1,n+1) scanf("%d",b+i);
-		build(1,1,n);
-		rep(i,1,m+1) {
-			scanf("%s%d%d",s,&l,&r);
-			if (s[0]=='a') {
-				modify(1,1,n,l,r);
-			} else {
-				printf("%d\n",query(1,1,n,l,r));
-			}
-		}
+//		for(int i=0;i<4*n;i++){
+//			cout<<tree[i].flag<<" "<<tree[i].maxa<<" "<<tree[i].minb<<" "<<tree[i].sum<<endl;
+//		}
 	}
 }
- 
+
+
