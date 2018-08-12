5a90228c71f59476e9404c58c6258d96ae382581
leonyan18 <33154558+leonyan18@users.noreply.github.com>
2018-08-12 12:30:45 +0800

    new

--- test.cpp
+++ test.cpp
@@ -1,34 +1,23 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <math.h>
 using namespace std;
-const double PI=acos(-1.0);
 typedef long long ll;
-#define rep(i,a,n) for (int i=a;i<n;i++)
-#define per(i,a,n) for (int i=n-1;i>=a;i--)
-#define clr(x) memset(x,0,sizeof(x))
-int main(){
-	//本地测试
-	#ifdef ONLINE_JUDGE
-	#else
-    freopen("C://Users//yan//Desktop//in.txt","w",stdout);
-	#endif
-	ios::sync_with_stdio(false);//取消同步
-	std::cin.tie(0);//解除cin与cout的绑定，进一步加快执行效率。
-	srand((unsigned)time(NULL));
-	cout<<"1000 1000"<<endl;
-	rep(i,0,1000){
-		rep(j,0,1000){
-			char c;
-			if(rand()%2==0){
-				c='A'+rand()%26;
-				cout<<c;
-			}
-			else{
-				c='a'+rand()%26;
-				cout<<c;
-			}
-		}
-		cout<<endl;
-	} 
+ll isPrime(ll n)
+{	//返回1表示判断为质数，0为非质数，在此没有进行输入异常检测
+	double n_sqrt;
+	if(n==2 || n==3) return 1;
+	if(n%6!=1 && n%6!=5) return 0;
+	n_sqrt=floor(sqrt((double)n));
+	for(ll i=5;i<=n_sqrt;i+=6)
+	{
+	    if(n%(i)==0 | n%(i+2)==0) return 0;
+	}
+        return 1;
+} 
+int main()
+{
+	int flag;
+	flag=isPrime(998244351);
+	cout<<flag<<endl;
+	return 0;
 }
-
-
---
+++ 牛客网多校8B.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+typedef long long LL;
+const int MOD = 998244353;
+ 
+int main() {
+#ifndef ONLINE_JUDGE
+//    freopen("","r",stdin);
+    freopen(R"(D:\ACM\CLionProject\code\nower_coder\Multi_University_Training_Contest\2018\eight\output.txt)", "w",
+            stdout);
+#endif
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<LL> inv(n + 5ULL), ans(n + 5ULL);
+    inv[0] = inv[1] = ans[0] = ans[1] = 1;
+    for (int i = 2; i <= n; i++)
+        inv[i] = (MOD - MOD / i) * inv[MOD % i] % MOD;
+    for (int i = 2; i <= n; i++)
+        ans[i] = ((6 * i - 3) * ans[i - 1] % MOD - (i - 2) * ans[i - 2] % MOD + MOD) % MOD * inv[i + 1] % MOD;
+    if(n == 1) cout << 1 << endl;
+    else cout << ans[n - 1] * 2 % MOD << endl;
+    return 0;
+}
