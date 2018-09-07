eed3cd0f53fdde7497cd99180cc52277e3d86ede
yan <33154558+leonyan18@users.noreply.github.com>
2018-09-07 14:38:55 +0800

    1

--- 2018网络.cpp
+++ 2018网络.cpp
@@ -7,29 +7,38 @@ typedef long long ll;
 #define clr(x) memset(x,0,sizeof(x))
 const int INF=0x3f3f3f3f;
 const int maxn=1000005;
-bool flag[1005]; 
 int main(){
 	//本地测试
 	#ifdef ONLINE_JUDGE
 	#else
-//    freopen("C://Users//yan//Desktop//in.txt","r",stdin);
-//    freopen("C://Users//yan//Desktop//out.txt","w",stdout);
+    freopen("C://Users//yan//Desktop//in.txt","r",stdin);
 	#endif
 	ios::sync_with_stdio(false);//取消同步
 	std::cin.tie(0);//解除cin与cout的绑定，进一步加快执行效率。
-	ll n;
-	cin>>n;
-	if(n==7){
-		cout<<10<<endl;
-	} 
-	if(n==77){
-		cout<<94<<endl;
-	} 
-	if(n==777){
-		cout<<892<<endl;
-	}
-	else{
-		cout<<shabi<<endl;
+	int t;
+	cin>>t;
+	while(t--){
+		int n,a;
+		cin>>n>>a;
+		if(n>2||n==0){
+			cout<<"-1 -1"<<endl;
+		}else if(n==2){
+			int flag=0;
+			rep(i,1,8000002){
+				int num=sqrt(a*a+i*i);
+				if(num*num==a*a+i*i){
+					cout<<i<<" "<<num<<endl;
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0){
+				cout<<-1<<" "<<-1<<endl;
+			}
+		}
+		else{
+			cout<<1<<" "<<1+a<<endl;
+		}
 	} 
 }
 
